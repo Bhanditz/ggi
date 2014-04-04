a6fc4d4306be0cab4bef7ac5c2bc1fb359ca40a3
JRice <jrice@eol.org>
2014-04-04 12:36:52 -0400

    Just a note

--- spec/models/node_spec.rb
+++ spec/models/node_spec.rb
@@ -17,6 +17,8 @@ describe Node do
 
       it { should respond_to :json }
 
+      # NOTE - this depends on the format of the file; we're assuming it's
+      # stable for now.
       it 'has the name' do
         expect(subject.json["scientificName"]).to eq("Mammalia")
       end
