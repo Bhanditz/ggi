300efcef77cff6324b0bfc3b82d4c7e3f2699960
JRice <jrice@eol.org>
2014-05-23 17:06:18 -0400

    Adding a few specs for the Uri class we added

--- spec/lib/ggi_uri_spec.rb
+++ spec/lib/ggi_uri_spec.rb
@@ -1,15 +1,47 @@
 describe Ggi::Uri do
 
-  it 'knows BHL' do
-    expect(Ggi::Uri.bhl.short_name).to eq('BHL')
-    expect(Ggi::Uri.bhl.long_name).to eq('BHL pages')
-    expect(Ggi::Uri.bhl.uri).to eq('http://eol.org/schema/terms/NumberReferencesInBHL')
+  before(:all) do
+    @known_uris = {
+      'BHL' => 'http://eol.org/schema/terms/NumberReferencesInBHL',
+      'BOLD' => 'http://eol.org/schema/terms/NumberPublicRecordsInBOLD',
+      'EOL' => 'http://eol.org/schema/terms/NumberRichSpeciesPagesInEOL',
+      'GBIF' => 'http://eol.org/schema/terms/NumberRecordsInGBIF',
+      'NCBI' => 'http://eol.org/schema/terms/NumberOfSequencesInGenBank',
+      'GGBN' => 'http://eol.org/schema/terms/NumberSpecimensInGGBN'
+    }
+
+    @long_names = {
+      'BHL' => 'BHL pages',
+      'BOLD' => 'BOLD records',
+      'EOL' => 'EOL rich pages',
+      'GBIF' => 'GBIF records',
+      'NCBI' => 'GenBank sequences',
+      'GGBN' => 'GGBN records'
+    }
   end
 
-  it 'knows BOLD' do
-    expect(Ggi::Uri.bold.short_name).to eq('BOLD')
-    expect(Ggi::Uri.bold.long_name).to eq('BOLD records')
-    expect(Ggi::Uri.bold.uri).to eq('http://eol.org/schema/terms/NumberPublicRecordsInBOLD')
+  ['BHL', 'BOLD', 'EOL', 'GBIF', 'NCBI', 'GGBN'].each do |name|
+    describe name do
+      subject { Ggi::Uri.send(name.downcase.to_sym) }
+      it "knows the short name" do
+        expect(subject.short_name).to eq(name)
+      end
+      it "knows the long name" do
+        expect(subject.long_name).to eq(@long_names[name])
+      end
+      it "knows the short name" do
+        expect(subject.uri).to eq(@known_uris[name])
+      end
+    end
+  end
+
+  it 'lists all URIs' do
+    @known_uris.values.each do |uri|
+      expect(Ggi::Uri.uris).to include uri
+    end
+  end
+
+  it 'lists all short names' do
   end
 
 end
