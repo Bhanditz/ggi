f3993cb4f878c2351c759331dd1d1abbed075700
JRice <jrice@eol.org>
2014-06-09 12:58:37 -0400

    Added icon for info.

--- config/compass.rb
+++ config/compass.rb
@@ -3,7 +3,7 @@ require 'compass-normalize'
 require 'breakpoint'
 
 project_path               = File.expand_path('../../', __FILE__)
-images_dir                 = "app/images"
+images_dir                 = "images"
 sass_dir                   = "app/css"
 http_generated_images_path = "/images"
 disable_warnings           = true
