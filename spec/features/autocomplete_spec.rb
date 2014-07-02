d5339abbb056f55a56d24ebcd687efe584b0674c
JRice <jrice@eol.org>
2014-07-02 14:42:23 -0400

    Rudimentary spec for autocomplete batch_size

--- spec/features/autocomplete_spec.rb
+++ spec/features/autocomplete_spec.rb
@@ -1,4 +1,10 @@
 describe 'autocomplete', js: true do
+
+  def expect_number_of_children(which, count)
+    expect(page).to have_selector("#{which}:nth-child(#{count})")
+    expect(page).to_not have_selector("#{which}:nth-child(#{count+1})")
+  end
+
   it 'shows the proper results' do
     visit '/'
     expect(page).to have_no_xpath '//ul[@id="ui-id-1"]/li[1]'
@@ -33,4 +39,12 @@ describe 'autocomplete', js: true do
     fill_in('search_term', with: 'nonsense')
     expect(page).to have_xpath '//ul[@id="ui-id-1"]/li[1]', text: 'No results'
   end
+
+  it 'returns the default batch size' do
+    visit '/'
+    expect(page).to have_no_xpath '//ul[@id="ui-id-1"]/li[1]'
+    fill_in('search_term', with: 'b')
+    expect_number_of_children("ul.ui-autocomplete li", 7)
+  end
+
 end
