a9a1f8cb193913df58719111569aa23641c622ea
JRice <jrice@eol.org>
2015-03-03 14:51:14 -0500

    Specs pass... the data changed, is all. Bad tests. I didn't make them more robust, though

--- spec/features/autocomplete_spec.rb
+++ spec/features/autocomplete_spec.rb
@@ -18,19 +18,11 @@ describe 'autocomplete', js: true do
   it 'shows the matched name when necessary' do
     visit '/'
     expect(page).to have_no_xpath '//ul[@id="ui-id-1"]/li[1]'
-    fill_in('search_term', with: 'birds')
-    # Since birds was the search term, and Birds will be displayed with the
-    # search result, we don't need to show an alternative name
-    expect(page).to have_xpath '//ul[@id="ui-id-1"]/li[1]', text: 'Eukaryota > Tetrapoda'
-    expect(page).to have_xpath '//ul[@id="ui-id-1"]/li[1]', text: 'Aves'
-    expect(page).to have_xpath '//ul[@id="ui-id-1"]/li[1]', text: 'Birds'
-    expect(page).to have_no_xpath '//ul[@id="ui-id-1"]/li[1]', text: 'Alternative name'
-    # the search term 'birds' is not part of the scientific name or 'bird-of-paradise'
-    # so we show an 'alternative name' which is the actual match that includes 'birds'
-    expect(page).to have_xpath '//ul[@id="ui-id-1"]/li[2]', text: 'Eukaryota > Passeriformes'
-    expect(page).to have_xpath '//ul[@id="ui-id-1"]/li[2]', text: 'Paradisaeidae'
-    expect(page).to have_xpath '//ul[@id="ui-id-1"]/li[2]', text: 'Bird-of-paradise'
-    expect(page).to have_xpath '//ul[@id="ui-id-1"]/li[2]', text: 'Alternative name: Birds of paradise'
+    fill_in('search_term', with: 'racc')
+    expect(page).to have_xpath '//ul[@id="ui-id-1"]/li[1]', text: 'Eukaryota > Carnivora'
+    expect(page).to have_xpath '//ul[@id="ui-id-1"]/li[1]', text: 'Procyonidae'
+    expect(page).to have_xpath '//ul[@id="ui-id-1"]/li[1]', text: 'Coatis, raccoons, and relatives'
+    expect(page).to have_xpath '//ul[@id="ui-id-1"]/li[1]', text: 'Alternative name: Raccoon, coati & olingo'
   end
 
   it 'shows a message when there are no results' do
